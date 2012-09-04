61ac7be2f35afc216417d767cc62ee382534bc74
Amit Erandole <amit.erandole@gmail.com>
2012-09-04 18:15:40 +0530

    updated link for logo link

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -47,6 +47,8 @@ describe "Static pages" do
     click_link "home"
     click_link "Sign up now"
     page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
 
   end
 
